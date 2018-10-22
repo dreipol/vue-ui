4bb02d3a178df84cf85fb15e5ee765cfd831ab99
fabs <faebeee@gmail.com>
2018-10-22 15:08:14 +0200

    Add simple filters

---
+++ src/filters/bgImage/index.js
@@ -0,0 +1,8 @@
+/**
+ * Wraps the image url in a vue style object
+ * @param {string} url - The url for the background-image
+ * @return {object} An object that can be used within a vue template's `:style` attribute
+ */
+export default function bgImageFilter(url) {
+    return { 'background-image': `url(${ url })` };
+}
