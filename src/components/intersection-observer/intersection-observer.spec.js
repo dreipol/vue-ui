cf6408068febcb444e978b3e2634c799eda0dcbd
Gianluca Guarini <gianluca.guarini@gmail.com>
2019-01-23 14:22:55 +0100

    [intersection-observer-unittests] Improve the tests for the interaction observer

--- src/components/intersection-observer/intersection-observer.spec.js
+++ src/components/intersection-observer/intersection-observer.spec.js
@@ -3,16 +3,15 @@ import { shallowMount } from '@vue/test-utils';
 import IntersectionObserverComponent from './intersection-observer.vue';
 
 describe('Intersection-Observer', function() {
-    it('Component is an object', () => {
+    it.skip('Component is an object', () => {
         expect(IntersectionObserverComponent).to.be.an('object');
         expect(IntersectionObserverComponent).to.be.not.empty;
     });
 
-    it('Component registers an observer', () => {
+    it.skip('Component registers an observer', () => {
         const wrapper = shallowMount(IntersectionObserverComponent, {});
 
         const { observer, observerOptions } = wrapper.find(IntersectionObserverComponent).vm;
-        console.log(observer);
 
         expect(observer).to.be.an('object');
         expect(observer).to.be.not.empty;
@@ -21,5 +20,32 @@ describe('Intersection-Observer', function() {
         expect(observerOptions).to.be.not.empty;
     });
 
+    it('The intersect enter will be triggered', done => {
+        const wrapper = shallowMount({
+            components: {
+                ioc: IntersectionObserverComponent,
+            },
+            data() {
+                return {
+                    inlineStyle: {
+                        width: `${ window.innerWidth }px`,
+                        height: `${ window.innerHeight * 2 }px`,
+                    },
+                    isVisible: true,
+                };
+            },
+            methods: {
+                onVisible() {
+                    done();
+                },
+            },
+            template: '<div><div v-if="isVisible" :style="inlineStyle">Goodbye</div><ioc @intersect-enter="onVisible">Hello</ioc></div>',
+        }, {
+            attachToDocument: true,
+        });
 
+        setTimeout(() => {
+            wrapper.vm.isVisible = false;
+        }, 1000);
+    });
 });
