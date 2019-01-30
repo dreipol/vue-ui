5debab26350b2d4ae3899de69e65695f85d893ba
fabs <faebeee@gmail.com>
2019-01-30 11:21:47 +0100

    Update unittests

--- src/components/intersection-observer/intersection-observer.spec.js
+++ src/components/intersection-observer/intersection-observer.spec.js
@@ -4,7 +4,7 @@ import IntersectionObserverComponent from './intersection-observer.vue';
 
 describe('Intersection-Observer', function() {
     it('Component is an object', () => {
-        expect(IntersectionObserverComponent).to.be.an('object');
+        expect(IntersectionObserverComponent).to.be.a('object');
         expect(IntersectionObserverComponent).to.be.not.empty;
     });
 
@@ -12,11 +12,9 @@ describe('Intersection-Observer', function() {
         const wrapper = shallowMount(IntersectionObserverComponent, {});
 
         const { observer, observerOptions } = wrapper.find(IntersectionObserverComponent).vm;
+        expect(observer).to.be.a('IntersectionObserver');
 
-        expect(observer).to.be.an('object');
-        expect(observer).to.be.not.empty;
-
-        expect(observerOptions).to.be.an('object');
+        expect(observerOptions).to.be.a('object');
         expect(observerOptions).to.be.not.empty;
     });
 
