21c1afa34042bf53bde242166eb24460b19353f0
Rouven Bühlmann <rouven.buehlmann@dreipol.ch>
2019-02-04 19:59:18 +0100

    Fix helper function

--- src/util/count-virtual-tags/index.js
+++ src/util/count-virtual-tags/index.js
@@ -1,8 +1,8 @@
 /**
  * Return the amount of virtual tags found in a slot list
- * @param {Array<Vue.slots>} slots - all the slots we need to count
+ * @param {Array<Vue.slots>} scopedSlots - all the slots we need to count
  * @return {number} the amount of the slots wrapped in a tag
  */
-export default function countVirtualTags(slots) {
-    return (slots && slots.filter(s => s.tag).length) || 0;
+export default function countVirtualTags(scopedSlots) {
+    return Array.isArray(scopedSlots) ? scopedSlots.filter(fn => fn()).length : 0;
 }
