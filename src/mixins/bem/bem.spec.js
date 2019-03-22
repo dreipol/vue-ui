dfc335bd41cd2a95e80591020ca1d9bf8b7876f2
nirazul <rouven.buehlmann@dreipol.ch>
2019-03-22 15:07:44 +0100

    Improve test coverage

--- src/mixins/bem/bem.spec.js
+++ src/mixins/bem/bem.spec.js
@@ -4,28 +4,53 @@ import { createLocalVue } from '@vue/test-utils';
 
 const localVue = createLocalVue();
 
-function getDummyComponentProps(mixin) {
+function getDummyComponentProps(mixin, customOptions = {}) {
     return {
         mixins: [mixin],
+        ...customOptions,
     };
 }
 
 describe('Mixin bem', () => {
+    describe('Props', () => {
+        it('behaves the same when `useProp` is false', () => {
+            const mixin = bemMixin('root', { useProp: false });
+            const vm = new localVue(getDummyComponentProps(mixin, {
+                data() {
+                    return { facets: [] };
+                },
+            }));
+
+            expect(vm.bemFacets).to.be.an('array');
+            expect(vm.bemFacets).to.include('root__base');
+        });
+        it('uses an internal data property when `useProp` is false', () => {
+            const mixin = bemMixin('root', { useProp: false });
+            const vm = new localVue(getDummyComponentProps(mixin, {
+                data() {
+                    return { facets: ['foo'] };
+                },
+            }));
+
+            expect(vm.bemFacets).to.be.an('array');
+            expect(vm.bemFacets).to.include('root__foo');
+        });
+    });
     describe('Computed properties', () => {
-        it('It returns the bemRoot class properly', () => {
+        it('returns the bemRoot class properly', () => {
             const vm = new localVue(getDummyComponentProps(bemMixin('root')));
 
             expect(vm.bemRoot).to.be.equal('root');
         });
 
-        it('It returns the bem facets properly', () => {
+        it('returns the bem facets properly', () => {
             const vm = new localVue(getDummyComponentProps(bemMixin('root')));
 
             expect(vm.bemFacets).to.be.an('array');
             expect(vm.bemFacets).to.include('root__base');
         });
 
-        it('Custom bem mixin options have also an impact on the computed pros', () => {
+        it('takes into consideration custom bem mixin options when constructing css classes', () => {
             const customOptions = {
                 bemModifierMarker: '$$',
                 defaultFacet: 'foo',
