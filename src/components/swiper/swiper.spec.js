841ab2c0b5d9120edd924538c7d5c0ea5e6dd4b3
jessicachrist <jessica.christ@dreipol.ch>
2020-06-03 14:49:27 +0200

    [implement-swiper] Add basic implementation and description

---
+++ src/components/swiper/swiper.spec.js
@@ -0,0 +1,3 @@
+import { expect } from 'chai';
+import { shallowMount } from '@vue/test-utils';
+import Swiper from './swiper';
---
+++ src/components/swiper/swiper.vue
@@ -0,0 +1,55 @@
+<template>
+    <!-- Slider main container -->
+    <div class="swiper-container">
+        <!-- Additional required wrapper -->
+        <div class="swiper-wrapper">
+            <!-- Slides -->
+            <slot name="slides"/>
+        </div>
+        <!-- If we need pagination -->
+        <slot name="pagination">
+            <div class="swiper-pagination"></div>
+        </slot>
+
+        <!-- If we need navigation buttons -->
+        <slot name="navigation">
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </slot>
+
+        <!-- If we need scrollbar -->
+        <slot name="scrollbar">
+            <div class="swiper-scrollbar"></div>
+        </slot>
+    </div>
+</template>
+
+<script>
+    import Swiper from 'swiper';
+
+    const DEFAULT_OPTIONS = {
+        simulateTouch: false,
+        preventInteractionOnTransition: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+    }
+
+    export default {
+        mounted() {
+            this.swiper = new Swiper(this.$el, {
+                ...DEFAULT_OPTIONS,
+                ...this.$attrs
+            });
+        },
+        beforeDestroy() {
+            this.swiper.destroy();
+        }
+    };
+</script>
