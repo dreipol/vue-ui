c35b18f4a28bc5e7838a4a39dfcefec5bf8020ef
jessicachrist <jessica.christ@dreipol.ch>
2020-11-12 11:18:11 +0100

    [implement-swiper] Add tests

--- src/components/swiper/swiper.spec.js
+++ src/components/swiper/swiper.spec.js
@@ -1,3 +1,22 @@
 import { expect } from 'chai';
-import { shallowMount } from '@vue/test-utils';
-import Swiper from './swiper';
+import Swiper from './swiper.vue';
+import { mount } from '@vue/test-utils';
+
+describe('Component swiper', () => {
+    it('is an object', () => {
+        expect(Swiper).to.be.an('object');
+        expect(Swiper).to.be.not.empty;
+    });
+
+    it('passes attrs props to swiper config', () => {
+        const wrapper = mount(Swiper, {
+            propsData: {
+                pagination: {
+                    clickable: false,
+                },
+            },
+        });
+        expect(wrapper.find('.swiper-pagination-bullet')).to.be.ok;
+        expect(wrapper.vm.$attrs.pagination.clickable).to.be.equal(false);
+    });
+});
