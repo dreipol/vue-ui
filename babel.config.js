0384275c89cb8fdeb84089eefa039d9ace9960e5
Fabrice Tobler <fabrice@tobler.dreipol.ch>
2020-11-12 13:57:28 +0100

    [tabs] Rework tabs code

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = {
     plugins: [
         '@babel/plugin-proposal-object-rest-spread',
         '@babel/plugin-proposal-export-namespace-from',
+        'transform-vue-jsx',
     ],
     env: {
         test: {
