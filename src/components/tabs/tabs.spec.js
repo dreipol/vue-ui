5167e295cb2c48689365ac5cf4d1da1a07b30093
Fabrice Tobler <fabrice@tobler.dreipol.ch>
2020-11-12 13:57:28 +0100

    [tabs] Rework tabs code

--- src/components/tabs/tabs.spec.js
+++ src/components/tabs/tabs.spec.js
@@ -1,58 +1,49 @@
 import UiTabs from './tabs.vue';
-import UiTab from './tab.vue';
 import { expect } from 'chai';
-import { shallowMount, mount } from '@vue/test-utils';
+import { mockData } from './mock-data';
+import { mount } from '@vue/test-utils';
 
 const css = `
-.ui-tabs--content-wrapper, .ui-tabs--content-wrapper__is-active {
-        position: relative;
-        opacity: 0;
+.ui-tabs--list {
+    display: flex;
 }
-.ui-tabs--content-wrapper__is-closing {
+.ui-tabs--list-item {
+    color: rgba(0, 0, 0, 0.5)
+}
+.ui-tabs--list-item__active {
+    color: rgba(0, 0, 0, 1)
+}
+.ui-tabs--pannel-wrapper {
+    position: relative;
+    transition: height 1s;
+}
+.ui-tabs--panel-enter, .ui-tabs--panel-leave-to {
     opacity: 0;
-    transition: opacity 500ms;
 }
-.ui-tabs--content-wrapper__is-opening {
-    opacity: 1;
-    transition: opacity 500ms;
-}
-
-.ui-tabs--content-wrapper__is-active {
-    opacity: 1;
-}
-.ui-tabs--link, .ui-tabs--link__is-active {
-    color: rgba(0, 0, 0, 1);
-}
-
-.ui-tabs--link__is-entering, .ui-tabs--link__is-leaving {
-    transition: color 500ms;
-    color: rgba(0, 0, 0, 0.5);
+.ui-tabs--panel-enter-active, .ui-tabs--panel-leave-active {
+    transition: all 1s;
+    position: absolute;
 }
 `;
 
-const TabContent = {
-    template: `<p>Hello Body</p>`,
-};
-const TabLabel = {
-    template: `<h1>Hello Tab</h1>`,
-};
-
-const contentWrapper = shallowMount(TabContent).vnode;
-const labelWrapper = shallowMount(TabLabel).vnode;
-
-const TabMock = ({ tabId, hasAnimation } = { tabId: null, hasAnimation: null }) => {
-    const tab = shallowMount(UiTab, {
-        propsData: {
-            tabId: tabId,
-            hasAnimation: hasAnimation,
+const mountWrapper = (props = {}) => mount(UiTabs, {
+    propsData: { ...props },
+    scopedSlots: {
+        tab: '<div slot-scope="props">{{ props.title }}</div>',
+        content() {
+            return [
+                mockData.map((data, index) => {
+                    return (
+                        <div title={data.tab.title} key={index}>
+                            <h1>{data.content.title}</h1>
+                            <p>{data.content.description}</p>
+                        </div>
+                    );
+                }),
+            ];
         },
-    });
-    tab.vm.$slots['tab-label'] = labelWrapper;
-    tab.vm.$slots['tab-body'] = contentWrapper;
-    
-    return tab.vm;
-};
-
+    },
+});
 
 describe('Component Tabs', () => {
     const styleNode = document.createElement('style');
@@ -72,141 +63,36 @@ describe('Component Tabs', () => {
         expect(UiTabs).to.be.not.empty;
     });
     it('renders Tabs', () => {
-        const wrapper = mount(UiTabs);
-        wrapper.setData({ initTabs: [TabMock(), TabMock(), TabMock()] });
-        expect(wrapper.find('h1').element.innerText).to.be.equal('Hello Tab');
-    });
-    it('fires activeTab on click', () => {
-        const wrapper = mount(UiTabs, {
-        });
-        wrapper.setData({ initTabs: [TabMock(), TabMock(), TabMock()] });
-        wrapper.findAll('.ui-tabs--link').wrappers[1].element.click();
-        expect(wrapper.vm.tabs[1].isActive).to.be.equal(true);
-    });
-    it('sets state.entering.tab by clicking on tab', () => {
-        const wrapper = mount(UiTabs, {
-            propsData: {
-                selectedTabId: 1,
-            },
-        });
-        wrapper.setData({ initTabs: [TabMock({ tabId: 1 }), TabMock(), TabMock()] });
-        expect(wrapper.vm.state.isEntering.tab).not.to.be.equal(null);
-    });
-    it('dos not set state.entering.tab if no active Tab', () => {
-        const wrapper = mount(UiTabs);
-        wrapper.setData({ initTabs: [TabMock(), TabMock(), TabMock()] });
-        expect(wrapper.vm.state.isEntering.tab).to.be.equal(null);
-    });
-    it('has "is-entering" class on click event', () => {
-        const wrapper = mount(UiTabs, {
-            propsData: {
-                selectedTabId: 1,
-            },
-        });
-        wrapper.setData({ initTabs: [TabMock({ tabId: 1 }), TabMock(), TabMock()] });
-        const clickElement = wrapper.findAll('.ui-tabs--link').wrappers[2];
-        
-        clickElement.element.click();
+        const wrapper = mountWrapper();
 
-        expect(clickElement.element.classList.contains('ui-tabs--link__is-entering')).to.be.equal(true);
+        expect(wrapper.find('.ui-tabs--list-item').element).to.exist;
     });
-    it('has no "is-entering" class on onTransitionEnd event', () => {
-        const wrapper = mount(UiTabs, {
-            propsData: {
-                selectedTabId: 1,
-            },
-        });
-        wrapper.setData({ initTabs: [TabMock({ tabId: 1 }), TabMock(), TabMock()] });
-        const clickElement = wrapper.findAll('.ui-tabs--link').wrappers[2];
+    it('renders only one panel if isLazy is set to true', () => {
+        const wrapper = mountWrapper();
     
-        wrapper.vm.onTransitionEnd(clickElement.element);
-    
-        expect(clickElement.element.classList.contains('ui-tabs--link__is-entering')).to.be.equal(false);
+        expect(wrapper.findAll('.ui-tabs--panel-wrapper').wrappers.length).to.be.equal(1);
     });
-    it('has "is-leaving" class on click event', () => {
-        const wrapper = mount(UiTabs, {
-            propsData: {
-                selectedTabId: 1,
-            },
-        });
-        wrapper.setData({ initTabs: [TabMock({ tabId: 1 }), TabMock(), TabMock()] });
-        const lastActiveElement = wrapper.findAll('.ui-tabs--link').wrappers[0];
-        const clickElement = wrapper.findAll('.ui-tabs--link').wrappers[2];
-        
-        clickElement.element.click();
-        expect(lastActiveElement.element.classList.contains('ui-tabs--link__is-leaving')).to.be.equal(true);
+    it('renders all panels to dome if isLazy is set to false', () => {
+        const wrapper = mountWrapper({ isLazy: false });
+    
+        expect(wrapper.find('.ui-tabs--panel-wrapper').element.childNodes.length).to.be.equal(4);
     });
-    it('has no "is-leaving" class on onTransitionEnd event', () => {
-        const wrapper = mount(UiTabs, {
-            propsData: {
-                selectedTabId: 1,
-            },
-        });
-        wrapper.setData({ initTabs: [TabMock({ tabId: 1 }), TabMock(), TabMock()] });
-        const lastActiveElement = wrapper.findAll('.ui-tabs--link').wrappers[0];
-        const clickElement = wrapper.findAll('.ui-tabs--link').wrappers[2];
+    it('sets initial active tab', () => {
+        const wrapper = mountWrapper({ initActiveId: 2 });
     
-        clickElement.element.click();
-    
-        wrapper.vm.onTransitionEnd(lastActiveElement.element);
-
-        expect(lastActiveElement.element.classList.contains('ui-tabs--link__is-leaving')).to.be.equal(false);
+        const tabs = wrapper.findAll('.ui-tabs--list-item').wrappers;
+        
+        const activeIndex = tabs.findIndex(tab => tab.element.classList.contains('ui-tabs--list-item__is-active'));
+        
+        expect(activeIndex).to.be.equal(2);
     });
-    it('has "is-active" class when tab is active', () => {
-        const wrapper = mount(UiTabs, {
-            propsData: {
-                selectedTabId: 1,
-            },
-        });
-        wrapper.setData({ initTabs: [TabMock({ tabId: 1 }), TabMock(), TabMock()] });
-
-        expect(wrapper.findAll('.ui-tabs--link').wrappers[0].element.classList.contains('ui-tabs--link__is-active')).to.be.equal(true);
-    });
-    it('has not "is-active" class when tab is clicked and hasAnimation is true', () => {
-        const wrapper = mount(UiTabs, {
-            propsData: {
-                selectedTabId: 1,
-                hasAnimation: true,
-            },
-        });
-        wrapper.setData({ initTabs: [TabMock({ tabId: 1 }), TabMock(), TabMock()] });
-    
-        const clickElement = wrapper.findAll('.ui-tabs--link').wrappers[2];
-    
-        clickElement.element.click();
-    
-    
-        expect(clickElement.element.classList.contains('ui-tabs--link__is-active')).to.be.equal(false);
-    });
-    it('has "is-active" class when tab is clicked and hasAnimation is false', () => {
-        const wrapper = mount(UiTabs, {
-            propsData: {
-                selectedTabId: 1,
-                hasAnimation: false,
-            },
-        });
-        wrapper.setData({ initTabs: [TabMock({ tabId: 1 }), TabMock(), TabMock()] });
+    it('sets active tab on tab click', () => {
+        const wrapper = mountWrapper();
         
-        const clickElement = wrapper.findAll('.ui-tabs--link').wrappers[2];
+        const clickTab = wrapper.findAll('.ui-tabs--list-item').wrappers[3].element;
         
-        clickElement.element.click();
+        clickTab.click();
         
-        
-        expect(clickElement.element.classList.contains('ui-tabs--link__is-active')).to.be.equal(true);
-    });
-    it('will not override hasTransition if hasAnimation is set on tab component', () => {
-        const wrapper = mount(UiTabs, {
-            propsData: {
-                selectedTabId: 1,
-                hasAnimation: true,
-            },
-        });
-        wrapper.setData({ initTabs: [TabMock({ tabId: 1 }), TabMock({ hasAnimation: false }), TabMock()] });
-        
-        const clickElement = wrapper.findAll('.ui-tabs--link').wrappers[1];
-        
-        clickElement.element.click();
-        
-        expect(wrapper.vm.tabs[1].hasTransition).to.be.equal(false);
+        expect(clickTab.classList.contains('ui-tabs--list-item__is-active')).to.be.true;
     });
 });
--- src/components/tabs/tabs.vue
+++ src/components/tabs/tabs.vue
@@ -1,184 +1,65 @@
 <template>
     <div class="ui-tabs">
-        <ul class="ui-tabs--list">
-            <li class="ui-tabs--list-item" v-for="(tab, index) in tabs" :key="index">
-                <a :class="tabClasses(tab)"
-                        :href="tab.href"
-                        class="ui-tabs--link"
-                        tabindex="0"
-                        :target="tab.target"
-                        :rel="tab.rel"
-                        v-if="tab.href !== ''"
-                        ref="tabRefs"
-                        @click.prevent="(e) => activateTab(e, tab)"
-                        @keypress.enter="(e) => activateTab(e, tab)"
-                        @transitionend="onTransitionEnd"
-                        @transitionstart="onTransitionStart">
-                    <render-label :label="tab.$slots['tab-label']" v-if="tab.$slots['tab-label']"/>
-                </a>
-                <div :class="tabClasses(tab)"
-                        :href="tab.href"
-                        class="ui-tabs--link"
-                        tabindex="0"
-                        v-else
-                        ref="tabRefs"
-                        @click.prevent="(e) => activateTab(e, tab)"
-                        @keypress.enter="(e) => activateTab(e, tab)"
-                        @transitionend="onTransitionEnd"
-                        @transitionstart="onTransitionStart">
-                    <render-label :label="tab.$slots['tab-label']" v-if="tab.$slots['tab-label']"/>
-                </div>
+        <ul class="ui-tabs--list" role="tablist">
+            <li :class="tabClasses(tab)"
+                    class="ui-tabs--list-item"
+                    role="tab"
+                    :tabindex="index === activeId ? 0 : -1"
+                    v-for="(tab, index) in tabs"
+                    :key="index"
+                    @click.prevent="activeTab(tab)"
+                    @keypress.enter="activeTab(tab)">
+                <slot name="tab" v-bind="getTabData(tab)"/>
             </li>
         </ul>
-        <div class="ui-tabs--body" ref="content">
-            <slot/>
-        </div>
+        <ui-tab :tabs="tabs" :active-id="activeId" :is-lazy="isLazy"/>
     </div>
 </template>
 
 <script>
     import { bemMixin } from '../../mixins';
+    import UiTab from './tab.vue';
     
     export default {
         components: {
-            //If we want to render not just strings we need to render The label as a Vue Component
-            RenderLabel: {
-                functional: true,
-                render: (h, ctx) => ctx.props.label,
-            },
+            UiTab,
         },
         mixins: [
             bemMixin('ui-tabs'),
         ],
         props: {
-            selectedTabId: {
-                type: Number || null,
-                default: null,
+            initActiveId: {
+                type: Number,
+                default: 0,
             },
-            hasAnimation: {
+            isLazy: {
                 type: Boolean,
                 default: true,
-            },
-            shouldAlwaysRenderContent: {
-                type: Boolean,
-                default: false,
             },
         },
         data() {
             return {
-                initTabs: [],
-                tabs: [],
-                state: {
-                    // We need the corresponding tab from tabs to set the Body active
-                    // We need the element to set to identify the active tab
-                    isEntering: {
-                        tab: null,
-                        el: null,
-                    },
-                    isLeaving: {
-                        tab: null,
-                    },
-                },
+                activeId: this.initActiveId,
             };
         },
         computed: {
-            setTabs() {
-                if (this.tabs.length > 0) {
-                    return this.tabs;
-                }
-                this.initTabs.forEach(tab => {
-                    if (tab.tabId === this.selectedTabId && this.selectedTabId) {
-                        tab.isActive = true;
-                    }
-                    tab.shouldAlwaysRender = this.shouldAlwaysRenderContent;
-                    if (tab.hasAnimation === null) {
-                        tab.setHasAnimation = this.hasAnimation;
-                    }
-                });
-                
-                this.setContentHeight();
-                return this.initTabs;
-            },
-            getActiveItem() {
-                return this.tabs.find(tab => tab.isActive);
-            },
-            calcHeight() {
-                if (this.state.isEntering.tab === null) {
-                    return 0;
-                }
-                return this.state.isEntering.tab.$el.offsetHeight;
+            tabs() {
+                return this.$scopedSlots.content();
             },
         },
         methods: {
-            setContentHeight() {
-                const { content } = this.$refs;
-                
-                if (!content) {
-                    return;
-                }
-                
-                this.$nextTick(() => {
-                    content.style.height = `${ this.calcHeight }px`;
-                });
-            },
-            activateTab(e, nextVisibleTab) {
-                const currentlyVisibleTab = this.tabs.find(tab => tab.isActive);
-                
-                if (currentlyVisibleTab === nextVisibleTab) {
-                    return;
-                }
-                
-                nextVisibleTab.isActive = true;
-                nextVisibleTab.isAnimating = true;
-                
-                if (currentlyVisibleTab) {
-                    currentlyVisibleTab.isAnimating = true;
-                    currentlyVisibleTab.isActive = false;
-                }
-                
-                this.state.isEntering.tab = nextVisibleTab;
-                this.state.isEntering.el = e.target;
-                this.state.isLeaving.tab = currentlyVisibleTab;
-                this.setContentHeight();
-            },
             tabClasses(tab) {
-                const isEntering = this.hasAnimation && this.state.isEntering.tab === tab && tab.isAnimating && tab.hasTransition;
-                const isLeaving = this.hasAnimation && this.state.isLeaving.tab === tab && this.hasAnimation && tab.hasTransition;
-                const isActive = tab.isActive && !isEntering;
-                
+                const isActive = this.tabs.indexOf(tab) === this.activeId;
                 return [
-                    this.bemAdd(isEntering && 'is-entering', 'link'),
-                    this.bemAdd(isLeaving && 'is-leaving', 'link'),
-                    this.bemAdd(isActive && 'is-active', 'link'),
+                    this.bemAdd(isActive && 'is-active', 'list-item'),
                 ];
             },
-            onTransitionStart() {
-                //Calculate height so we can set the bodies position to absolute
-                const { content } = this.$refs;
-                content.style.height = `${ this.contentHeight }px`;
+            activeTab(tab) {
+                this.activeId = this.tabs.indexOf(tab);
             },
-            onTransitionEnd(e) {
-                const transitionTab = this.$refs.tabRefs.find(item => item === e.target);
-                const isEntering = transitionTab === this.state.isEntering.el;
-                if (isEntering) {
-                    this.state.isEntering.tab.isAnimating = false;
-                    return;
-                }
-                this.state.isLeaving.tab = null;
+            getTabData(tab) {
+                return tab.data ? tab.data.attrs : {};
             },
-        },
-        mounted() {
-            // Watch the tabs and find the active tab
-            this.$watch('getActiveItem', () => {
-                this.state.isEntering.tab = this.getActiveItem;
-            });
-            
-            //If we want to set Tabs after mounting we need to watch the tabs and set the active tab correctly
-            this.$watch('setTabs', () => {
-                this.tabs = this.setTabs;
-            });
-            
-            this.initTabs = this.$children;
         },
     };
 </script>
